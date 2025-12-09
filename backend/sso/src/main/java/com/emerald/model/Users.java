c82cfa10e30265471c674237f4dcf1f64da829de
scanalesR <scanales@rocketsoftware.com>
2025-12-09 16:05:07 -0500

    Update Entities with Column names

--- backend/sso/src/main/java/com/emerald/model/Users.java
+++ backend/sso/src/main/java/com/emerald/model/Users.java
@@ -1,10 +1,11 @@
 package com.emerald.model;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
@@ -12,8 +13,10 @@ import jakarta.persistence.Table;
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="userID")
     private int id;
 
+    @Column(name="User_Name")
     private String userName;
 
     public Users() {
