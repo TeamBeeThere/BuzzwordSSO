ee13256beca683bba9d3b56cf1a48b394bf67dce
scanalesR <scanales@rocketsoftware.com>
2025-12-09 21:52:04 -0500

    Update and correct annotations

--- backend/sso/src/main/java/com/emerald/sso/SsoApplication.java
+++ backend/sso/src/main/java/com/emerald/sso/SsoApplication.java
@@ -1,14 +1,21 @@
 package com.emerald.sso;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.emerald.config.SpringConfig;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.emerald")
+@ComponentScan(basePackages = {
+	"com.emerald.service",
+	"com.emerald.controller"
+})
+@EntityScan(basePackages = "com.emerald.model")
+@EnableJpaRepositories(basePackages = "com.emerald.repository")
 @Import(SpringConfig.class)
 public class SsoApplication {
 
