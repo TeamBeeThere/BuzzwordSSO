c1ca37a7b4aac29c8e8b0fda21446d213f29e759
Rocket Student <noone@nowhere.com>
2025-12-11 16:32:30 -0500

    CHANGES: Fixed docker, updated seed

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
+export const API_URL = 'http://localhost:6767/sso';
+=======
 export const API_URL = 'http://172.16.0.202:6767/sso';
+>>>>>>> 864162a (adding login support)
 
 export const getToken = () => localStorage.getItem('token');
 
